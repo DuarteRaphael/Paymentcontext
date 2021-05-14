64cc98bab427cdb1f83a9e8820f2e6633db7a7df
Raphael Duarte <raphael-duarte@outlook.com>
2021-05-14 02:52:16 -0300

    Adicionar arquivos de projeto.

---
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Domain.Entities
+{
+    public abstract class Payment
+    {
+        public DateTime PaidDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public decimal Total { get; set; }
+        public decimal TotalPaid { get; set; }
+    }
+    public class BoletoPayment
+    {
+
+    }
+    public class CreditCardPayment
+    {
+
+    }
+    public class PayPalPayment
+    {
+
+    }
+}
